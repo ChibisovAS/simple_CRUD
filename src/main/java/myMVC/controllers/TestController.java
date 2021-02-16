e0a91602d0c13c4b18b883d8d720844256d5a1f3
Andrey Chibisov <chibi_and@mail.ru>
2021-02-16 19:34:47 +0300

    add model

--- src/main/java/myMVC/controllers/TestController.java
+++ src/main/java/myMVC/controllers/TestController.java
@@ -1,6 +1,7 @@
 package myMVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class TestController {
 
     @GetMapping("/test")
-    public String test(@RequestParam(value = "name",required = false) String name) {
-        System.out.println(name + "!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    public String test(@RequestParam(value = "name",required = false) String name, Model model) {
+
+        model.addAttribute("name",name);
+
         return "view-test-dir/view-example";
     }
 }
